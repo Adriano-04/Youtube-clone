53d9fc91326470f57ddb343c2d304bfc2a910e48
Adriano <adrianomarque135@gmail.com>
2025-05-08 23:18:54 -0300

    correçoes e melhorias

--- src/app/video-page/video-page.component.ts
+++ src/app/video-page/video-page.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SideVideoCardComponent } from '../side-video-card/side-video-card.component';
+import { SideBarModalComponent } from '../side-bar-modal/side-bar-modal.component';
 
 @Component({
   selector: 'app-video-page',
-  imports: [HeaderComponent,SideVideoCardComponent],
+  imports: [HeaderComponent,SideVideoCardComponent,SideBarModalComponent],
   templateUrl: './video-page.component.html',
   styleUrl: './video-page.component.css'
 })
 export class VideoPageComponent {
+  MenuOpen = false;
 
+  toggleMenu() {
+    this.MenuOpen = !this.MenuOpen;
+  }
 }
